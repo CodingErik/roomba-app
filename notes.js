8413c1cc95899cffcbbc79e05d79d0dee6641bd6
Erik De Luna <codingerik1.gmail.com>
2020-10-22 10:47:00 -0500

    adding starting notes

---
+++ notes.js
@@ -0,0 +1,19 @@
+// * Roomba App
+// todo 
+
+// * user given input // this is what our program will take in  
+// ? this will be collected as a JSON, we can collect this from a form or something like this 
+    // room dimensions                  // type Array [x,y]  // [5,10]  5 units wide 10 units high
+    // intial location of the roomba    // type Array [x,y]
+    // dirt locations                   // type Array [x,y] // dirt location is specified like this 
+    // driving instructions             // given in an Array of string ['N','S','E','W']
+
+// * output expected from client         
+    // final location of the roomba     
+    // total amount of dirt collected    // dirtCollected type integer      // incremented by dirt collected
+    // total amount of wall hits         // wallHits type integer          // incremented by each wall hit
+    // total amount of distance traveled // distanceTraveled type integer  // incremented by each step 
+
+
+
+// ! roomba should not move but just record a wall hit 
