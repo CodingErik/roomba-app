feb4a1b5fb92843f6da9014fdd603fd66676836e
Erik De Luna <codingerik1.gmail.com>
2020-10-28 12:56:44 -0500

    Updates

---
+++ precache-manifest.51bcf81c37094801bdaa6c314aed6299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d636d336524fe5aebed458f92d29f722",
+    "url": "/roomba-app/index.html"
+  },
+  {
+    "revision": "9c2ad791225af4fb33b1",
+    "url": "/roomba-app/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "9c2ad791225af4fb33b1",
+    "url": "/roomba-app/static/js/2.21f41dc1.chunk.js"
+  },
+  {
+    "revision": "6331714525e01b6332950210a982bc41",
+    "url": "/roomba-app/static/js/2.21f41dc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d41c42ef1655d09fb9",
+    "url": "/roomba-app/static/js/main.ceeeb17d.chunk.js"
+  },
+  {
+    "revision": "ade5d9e2e780d907fd3d",
+    "url": "/roomba-app/static/js/runtime-main.a38dae84.js"
+  }
+]);
